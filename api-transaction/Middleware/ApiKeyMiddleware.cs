5c0e753be1c3378f52b98e7e6805b29150144086
Maximustres <luis.bustos18.lb@gmail.com>
2025-08-05 13:46:07 -0400

    Add: apikey middleware

---
+++ api-transaction/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace api_transaction.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string API_KEY_HEADER_NAME = "X-API-KEY";
+        private readonly string _expectedApiKey;
+        private readonly ILogger<ApiKeyMiddleware> _logger;
+
+        public ApiKeyMiddleware(
+            RequestDelegate next, 
+            IConfiguration configuration, 
+            ILogger<ApiKeyMiddleware> logger)
+        {
+            _next = next;
+            _expectedApiKey = configuration.GetValue<string>("ApiSettings:ApiKey");
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(API_KEY_HEADER_NAME, out var extractedApiKey))
+            {
+                _logger.LogWarning("Missing API key");
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await context.Response.WriteAsync("API Key is missing");
+                return;
+            }
+
+            if (!_expectedApiKey.Equals(extractedApiKey))
+            {
+                _logger.LogWarning("Invalid API key: {ExtractedApiKey}", extractedApiKey.ToString());
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await context.Response.WriteAsync("Invalid API Key");
+                return;
+            }
+
+            await _next(context);
+        }
+
+    }
+}
--- api-transaction/Program.cs
+++ api-transaction/Program.cs
@@ -34,6 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+app.UseMiddleware<ApiKeyMiddleware>();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
